43849ec27697dfaf4f9a94d8e5d2b2b9e0fa30bf
diegocampos1 <diegodiko05@gmail.com>
2021-10-10 22:32:52 -0300

    Remove uncessary import

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,9 +1,9 @@
 import { MailerModule } from '@nestjs-modules/mailer';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-import { Global, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { join } from 'path';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
