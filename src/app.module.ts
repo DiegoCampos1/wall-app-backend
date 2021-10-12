3fcaac8a7d40eca7fe2ae0fd80b16250fd791553
diegocampos1 <diegodiko05@gmail.com>
2021-10-11 21:16:38 -0300

    DB connect move to .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,12 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://db_user:78D5PhoFJx1NQSak@cluster0.d8pxw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    ),
     ConfigModule.forRoot({
       isGlobal: true, // no need to import into other modules
     }),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.d8pxw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    ),
     UsersModule,
     AuthModule,
     PostsModule,
