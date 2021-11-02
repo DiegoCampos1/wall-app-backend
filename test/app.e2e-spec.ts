0b1c579b6d6fa6c0a183373b98805be2e7f35da8
diegocampos1 <diegodiko05@gmail.com>
2021-11-02 14:06:07 -0300

    delete unsed file in app.e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,7 +2,6 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-import { UsersService } from 'src/users/shared/users.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
