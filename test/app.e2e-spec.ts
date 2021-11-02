012557d9c064b274505c190f106a06df1b4064fd
diegocampos1 <diegodiko05@gmail.com>
2021-11-02 14:03:43 -0300

    Feature test end2end implematation

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { UsersService } from 'src/users/shared/users.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -19,11 +20,31 @@ describe('AppController (e2e)', () => {
     await app.close();
   });
 
-  it('/users (GET)', () => {
-    return request(app.getHttpServer()).get('/users').expect(200);
+  describe('/users tests', () => {
+    it('/users (GET)', () => {
+      return request(app.getHttpServer()).get('/users').expect(200);
+    });
+
+    it('/users:id (GET NOTFOUND)', () => {
+      return request(app.getHttpServer()).get('/users/1').expect(404);
+    });
+
+    it('/users (POST)', () => {
+      return request(app.getHttpServer()).post('/users').expect(201);
+    });
   });
 
-  it('/users (POST)', () => {
-    return request(app.getHttpServer()).post('/users').expect(201);
+  describe('/posts tests', () => {
+    it('/posts (GET)', () => {
+      return request(app.getHttpServer()).get('/posts').expect(200);
+    });
+
+    it('/posts:id (POST NOTFOUND)', () => {
+      return request(app.getHttpServer()).get('/posts/1').expect(404);
+    });
+
+    it('/posts (POST Unauthorized)', () => {
+      return request(app.getHttpServer()).post('/posts').expect(401);
+    });
   });
 });
